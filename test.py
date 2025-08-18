9b32116bfb0931a555cb8e1b20c66cc6c40ae147
wata-nana <tonarinotototro@gmail.com>
2025-08-19 08:33:06 +0900

    テストコード追加

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
+from customer import Customer
 
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
+michelle = Customer(first_name="Michelle", family_name="Tanner", age=3)
+
+print(ken.full_name())  # "Ken Tanaka" という値を出力
+print(tom.full_name())  # "Tom Ford" という値を出力
+print(ieyasu.full_name())  # "Ieyasu Tokugawa" という値を出力
+
+print(ken.age)  # 15 という値を出力
+print(tom.age)  # 57 という値を出力
+print(ieyasu.age)  # 75 という値を出力
+
+print(ken.entry_fee())  # 1000 という値を出力
+print(tom.entry_fee())  # 1500 という値を出力
+print(ieyasu.entry_fee())  # 1200 という値を出力
+
+print(ken.info_csv())  # "Ken Tanaka,15,1000" という値を出力
+print(tom.info_csv())  # "Tom Ford,57,1500" という値を出力
+print(ieyasu.info_csv())  # "Ieyasu Tokugawa,75,1200" という値を出力
